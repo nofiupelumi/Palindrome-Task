0717dd1fdd607a86502dfff1c1b2690e036613cb
frontend project <nofiumoruf17@gmail.com>
2023-12-23 13:24:19 +0000

    i just added weeks 37,38,39,40 and some little changes in  other files.

---
+++ P38online.py
@@ -0,0 +1,1 @@
+class shifted till further notice
---
+++ p33cnvonline.py
@@ -0,0 +1,10 @@
+# # # You're a professor teaching Data Science with Python, and you want to visually assess if the grades on your exam follow a particular distribution. 
+# # # Which plot do you use?ans histogram
+
+# # You're a professor in Data Analytics with Python, and you want to visually assess if longer answers on exam questions lead to higher grades. Which plot do you use?
+
+# # ANS Scatter plot
+
+# f you have a look at your colorful plot, it's clear that people live longer in countries with a higher GDP per capita. No high income countries have really short life expectancy, and no low income countries have very long life expectancy. Still, there is a huge difference in life expectancy between countries on the same income level. Most people live in middle income countries where difference in lifespan is huge between countries; depending on how income is distributed and how it is used.
+
+# What can you say about the plot?
